4d9beaf1d1895b68cb7580cd2dc482aae2984687
JoelEngstrom <joel.n.engstrom@protonmail.com>
2023-05-03 16:20:34 +0200

    nothing works
    Co-authored-by: lisabybro <lisabybro@users.noreply.github.com>

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -16,14 +16,13 @@ def powerset(iterable):
     s = list(iterable)
     return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
 
-def leaf_nodes(tree, node = "1"):
-    if not node in tree:
-        leaf_nodes_list.append(node)
-        return True
-    children = tree[node]
 
-    for child in children:
+def leaf_nodes(tree, node="1"):
+    if node not in tree:
+        leaf_nodes_list.append(node)
+    for child in tree[node]:
         leaf_nodes(tree, child)
+
 
 def neighbours(ps):
     neighbours = set()
@@ -31,13 +30,13 @@ def neighbours(ps):
         neighbours.update(graph[s])
     return neighbours
 
+
 def checkIndependent(name, nodes, graphI):
     for node in nodes:
         if graphI[node] & set(nodes):
             return False
     independent_sets[name].add(nodes)
     return True
-
 
 
 def intersection(nodes, graphI):
@@ -61,7 +60,7 @@ for name, vertices in bags.items():
     max_val = 0
     independent_sets[name] = set()
     for s in ps:
-        if checkIndependent(name ,s, graph):
+        if checkIndependent(name, s, graph):
             max_val = max(max_val, len(s))
     weights[name] = max_val
 
@@ -75,10 +74,24 @@ pprint(leaf_nodes_list)
 for leaf in leaf_nodes_list:
     for s in independent_sets[leaf]:
         if not leaf in calculations:
-            calculations[leaf] = {s : len(s) }
+            calculations[leaf] = {s: len(s)}
         else:
-            calculations[leaf].update( {s : len(s)})
+            calculations[leaf].update({s: len(s)})
 
+
+def calculate(tree, ui, w, inde_set={}):
+    # base-case: rotnoden
+    if ui in leaf_nodes_list:
+        return calculations[ui]
+
+    for child in tree[ui]:
+        temp_set = inde_set.append(child)
+        if checkIndependent(temp_set):
+            return calculate(tree, child, len(temp_set), temp_set)
+
+    #
+    #
+    #
 
 
 print("Calc")

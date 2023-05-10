938ecf2fd8fa9cd47a15a50096b32b13a3757a26
Sebastian <sebastian.malmstrom@hotmail.se>
2023-05-10 09:16:25 +0200

    hehe

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -1,12 +1,20 @@
 from pprint import pprint
 import random
 
-filename = "pw09_100.9"
-content = open("./data/" + filename + ".txt", "r").readlines()
 
+
+
+file = "data\matching_1000.txt"
+vertices, weights = [], {}
+f = open(file)
+f.readline()
+for x in open(file).readlines():
+    tmp = x.split(" ")
+    if len(tmp) > 2:
+        vertices.append((tmp[0],tmp[1]))
+        weights[(tmp[0],tmp[1])] = tmp[2].strip()
 
 A = {}
-B = {}
 
 def assign(vertex):
     if random.randint(0, 1) == 1:
